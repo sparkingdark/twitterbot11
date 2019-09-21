17bfe6b08ac83b40ef2d13dd9bcb624b9232f466
darkdebo <50295688+darkdebo@users.noreply.github.com>
2019-09-21 22:12:19 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+// Put your own Twitter App keys here. See README.md for more detail.
+module.exports = {
+  consumer_key:         'eu3cqQ7ye9ZGfwk6ZrXjuZqcu',
+  consumer_secret:      '6puTjgBH0gBZrBFSgVHFWfpjPBDI2B5x2BFjrHBf88Fz2t1qeB',
+  access_token:         '1132851640057225216-feIvVbpu5HMoJgtCbz3GoZRIGqNThR',
+  access_token_secret:  'hoqb1jWE3xLeRVVGwG72399Z7qGCNIFm7rMrO0Og7avxg'
+}
