bee0f2b70aa5893e81652b198855b1e84128f87e
Debojyoti Chakraborty <50295688+darkdebo@users.noreply.github.com>
2020-03-08 20:52:25 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // Put your own Twitter App keys here. See README.md for more detail.
 module.exports = {
-  consumer_key:         'eu3cqQ7ye9ZGfwk6ZrXjuZqcu',
-  consumer_secret:      '6puTjgBH0gBZrBFSgVHFWfpjPBDI2B5x2BFjrHBf88Fz2t1qeB',
-  access_token:         '1132851640057225216-feIvVbpu5HMoJgtCbz3GoZRIGqNThR',
-  access_token_secret:  'hoqb1jWE3xLeRVVGwG72399Z7qGCNIFm7rMrO0Og7avxg'
+  consumer_key:         'OmpBWs8mHKiIFDbwJGke7T0uE',
+  consumer_secret:      'NBMddOT4LOmAaE4iKIjSMV1VyqTlWUY7TBgOYkHCQufyV8KdW5',
+  access_token:         '1175597608124862466-dxoZkX9mRcqOT3kd4SoF4AjmWZOw0W',
+  access_token_secret:  'ViY6P0Qnhb586rrQtbaYzbKpBi37iCnIoYvJi5G4hbWPo'
 }
